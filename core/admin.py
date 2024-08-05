0246ccc67c0af4d97efe4a7684e254a53eef8c2f
Rajarshi Samaddar <itsrajarshi@gmail.com>
2024-08-05 16:42:50 +0000

    chore: Initialize Django project structure

---
+++ core/admin.py
@@ -0,0 +1,20 @@
+# admin.py
+from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin as DefaultUserAdmin
+from django.utils.translation import gettext_lazy as _
+
+class FreeDailyHoroscopeAdmin(admin.AdminSite):
+    site_header = _('Free Daily Horoscope Admin')
+    site_title = _('Free Daily Horoscope Admin')
+    index_title = _('Welcome to Free Daily Horoscope Admin')
+
+# Create an instance of the custom admin site
+free_daily_horoscope_admin = FreeDailyHoroscopeAdmin(name='FreeDailyHoroscopeAdmin')
+
+# Ensure the User model is registered with the custom admin site
+free_daily_horoscope_admin.register(User, DefaultUserAdmin)
+
+# Register other models as needed
+# from .models import YourModel
+# admin_site.register(YourModel)
